a9d112b969d346918eca65dd9e38dc533e8aba9d
sudhanshu yadav <sudhanshuy991@gmail.com>
2026-06-10 21:25:29 +0530

    question 4

---
+++ question4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, n = 5;
+
+    for(i = 1; i <= n; i++) {
+
+        for(j = 1; j <= n - i; j++)
+            printf(" ");
+
+        for(j = 1; j <= i; j++)
+            printf("%c", 'A' + j - 1);
+
+        for(j = i - 1; j >= 1; j--)
+            printf("%c", 'A' + j - 1);
+
+        printf("\n");
+    }
+
+    return 0;
+}
